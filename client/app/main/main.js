27e3d1e78cadc585d9e8f96955efff6066fb446a
David Kim <innerb@gmail.com>
2015-09-05 18:06:26 -0700

    Initial 'yo angular-fullstack'

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('myngfullApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
